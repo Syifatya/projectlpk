4b6e22f69543bf250be262bd86493e739f7a2ddf
syifa athiyya <syifaathiyyaa@gmail.com>
2025-07-29 13:23:05 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def get_base64(file_path):
     return base64.b64encode(data).decode()
 
 # ubah sesuai nama file gambarmu
-bg_image = get_base64("E:/syifakuliah/coding/coba/backgroundkimia.jpg")
+bg_image = get_base64("backgroundkimia.jpg")
 
 # CSS & Gaya Imut Kimia
 s.markdown(f"""
